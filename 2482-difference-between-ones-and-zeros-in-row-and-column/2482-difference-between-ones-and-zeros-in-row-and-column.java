edd07309b767aa907c607d9481d8c3de0c167298
Prabhat Kumar Roy <royprabhat0003@googlemail.com>
2026-09-04 00:08:21 +0530

    Time: 9 ms (79.9%), Space: 179.8 MB (10.13%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] rowOnes = new int[m];
+        int[] colOnes = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowOnes[i]++;
+                    colOnes[j]++;
+                }
+            }
+        }
+
+        int[][] diff = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int rowZeros = n - rowOnes[i];
+                int colZeros = m - colOnes[j];
+
+                diff[i][j] = rowOnes[i] + colOnes[j]
+                           - rowZeros - colZeros;
+            }
+        }
+
+        return diff;
+    }
+}
